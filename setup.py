545339767e3d49aae7a74bb40a9a0cf1fca4db20
unknown <18uec169@lnmiit.ac.in>
2020-10-09 18:18:11 +0530

    Update 0.0.3

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 import cx_Freeze
-
-executables = [cx_Freeze.Executable('main.py')]
+#setUp
+executables = [cx_Freeze.Executable('ABitRacey.py')]
 
 cx_Freeze.setup(
     name = 'A Bit Racey',
     options = {"build_exe" : {"packages":["pygame"],
-                                "include_files":["car.jpeg"]}},
+                                "include_files":["car.jpeg","crashSound.wav"]
+                            }},
     executables = executables
 )
+#end
